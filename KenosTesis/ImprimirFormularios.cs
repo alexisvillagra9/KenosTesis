ad0c846f418a9262af51dd96f41e09bcc74ab344
alexisvillagra9 <alexisvillagra9@gmail.com>
2017-10-27 20:57:54 -0300

    test

--- KenosTesis/ImprimirFormularios.cs
+++ KenosTesis/ImprimirFormularios.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//comentario de prueba git
 namespace KenosTesis
 {
     public partial class ImprimirFormularios : Form
